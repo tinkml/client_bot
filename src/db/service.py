f3fa0975a1a31d67457a44fa41f1c0aed429a229
tinkml <1996ntishenko@gmail.com>
2021-06-23 20:33:24 +0500

    Реализ. базовый CRUD для БД

--- src/db/service.py
+++ src/db/service.py
@@ -10,10 +10,12 @@ class ModelService:
     MODEL = None
 
     @classmethod
-    async def create(cls, data: dict):
+    async def create(cls, data: dict) -> MODEL:
         async with db.async_session() as session:
-            session.add(cls.MODEL(**data))
+            obj = cls.MODEL(**data)
+            session.add(obj)
             await session.commit()
+            return obj
 
     @classmethod
     async def get_all(cls) -> List[MODEL]:
@@ -28,9 +30,19 @@ class ModelService:
             return result.scalars().first()
 
     @classmethod
-    async def update_from_dict(cls, data: dict) -> None:
-        ...
+    async def update_from_dict(cls, object_id: int, data: dict) -> None:
+        async with db.async_session() as session:
+            result = await session.execute(select(cls.MODEL).where(cls.MODEL.id == object_id))
+            obj = result.scalars().first()
+            for key, value in data.items():
+                if hasattr(obj, key):
+                    setattr(obj, key, value)
+            await session.commit()
 
     @classmethod
     async def delete_by_id(cls, object_id: int) -> None:
-        ...
+        async with db.async_session() as session:
+            result = await session.execute(select(cls.MODEL).where(cls.MODEL.id == object_id))
+            obj = result.scalars().first()
+            await session.delete(obj)
+            await session.commit()
